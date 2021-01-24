24e1cfa0868ece26c6cfc0e4f01280b460a840d7
Junko Mikami <junkomikami312nz@gmail.com>
2021-01-24 18:25:23 +0900

    Implement expiration date column to task and sort expiration date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_074111) do
+ActiveRecord::Schema.define(version: 2021_01_24_054434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_074111) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "expiration_date", default: "2021-01-24", null: false
   end
 
 end
