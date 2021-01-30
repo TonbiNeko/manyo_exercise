5b90374d954233425b5ee46e804e7ad1b5058698
Junko Mikami <junkomikami312nz@gmail.com>
2021-01-30 22:39:10 +0900

    Add priority column to task table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_29_060218) do
+ActiveRecord::Schema.define(version: 2021_01_30_133649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_01_29_060218) do
     t.datetime "updated_at", null: false
     t.date "expiration_date", default: "2021-01-24", null: false
     t.integer "status"
+    t.integer "priority"
   end
 
 end
