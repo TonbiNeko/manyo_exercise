7390786f929d67891494fab432f92242394ddbdb
Junko Mikami <junkomikami312nz@gmail.com>
2021-02-03 19:23:25 +0900

    Implement Signup, Log in and Logout

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       redirect_to @task, notice: "タスクを登録しました"
     else
